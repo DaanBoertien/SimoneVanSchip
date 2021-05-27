716b105baac018ed6564610e7189ba116a582ae8
daanb <daan.boertien@gmail.com>
2021-05-27 13:41:27 +0200

    add logos to cms

--- assets/js/st.js
+++ assets/js/st.js
@@ -3,7 +3,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 ScrollTrigger.batch('.fadeIn', {
-    onEnter: batch => gsap.to(batch, {duration: 1, y: 0, autoAlpha: 1, stagger: 0.2, ease: Power2.out})
+    onEnter: batch => gsap.to(batch, {duration: 1, y: 0, autoAlpha: 1, stagger: 0.25, ease: Power2.out})
 })
 
 ScrollTrigger.batch('.fadeInTriangle', {
